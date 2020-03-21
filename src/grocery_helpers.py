c4e13bc87e2165f9a8cf5a64015d50789faf31ff
Ryan Fobel <ryan@fobel.net>
2020-03-21 11:53:18 -0400

    Add preliminary support for walmart.ca

--- src/grocery_helpers.py
+++ src/grocery_helpers.py
@@ -11,6 +11,7 @@ from selenium.common.exceptions import (NoSuchElementException,
                                         ElementClickInterceptedException,
                                         StaleElementReferenceException)
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 
 
 class Timeout(Exception):
@@ -21,6 +22,21 @@ class NoSearchResults(Exception):
     pass
 
 
+def setup_and_teardown_driver(func):
+        def wrapper(*args, **kwargs):
+            self = args[0]
+            if self._driver:
+                close_driver = False
+            else:
+                self.init_driver()
+                close_driver = True
+            ret = func(*args, **kwargs)
+            if close_driver:
+                self.close_driver()
+            return ret
+        return wrapper
+
+    
 class GroceryHelpersAPI:
 
     def __init__(self, user=None, password=None, user_data_dir=None,
@@ -39,7 +55,7 @@ class GroceryHelpersAPI:
         self._base_url = base_url
 
     def __del__(self):
-        self._close_driver()
+        self.close_driver()
         
     def init_driver(self, headless=False):
         options = webdriver.ChromeOptions()
@@ -52,27 +68,12 @@ class GroceryHelpersAPI:
             options.add_argument('headless')
 
         self._driver = webdriver.Chrome(options=options)
-        self._driver.get(self._base_url)
 
     def close_driver(self):
         if self._driver:
             self._driver.close()
             self._driver = None
 
-    def setup_and_teardown_driver(func):
-        def wrapper(*args, **kwargs):
-            self = args[0]
-            if self._driver:
-                close_driver = False
-            else:
-                self.init_driver()
-                close_driver = True
-            ret = func(*args, **kwargs)
-            if close_driver:
-                self.close_driver()
-            return ret
-        return wrapper
-    
     @setup_and_teardown_driver
     def search(self, term, timeout=10, follow_first_link=False):
         self._driver.get('%s/search?search-bar=%s' % (self._base_url, term))
@@ -81,11 +82,16 @@ class GroceryHelpersAPI:
 
         items = []
         while len(items) == 0 and time.time() - start_time < 10:
-            items = self._driver.find_elements_by_class_name('product-tile-group__item')
-            product_data = [json.loads(item.find_element_by_class_name('product-tracking').get_attribute('data-track-products-array')) for item in items]
-            links = self._driver.find_elements_by_class_name('product-tile__details__info__name__link')
+            items = self._driver.find_elements_by_class_name(
+                'product-tile-group__item')
+            product_data = [json.loads(item.find_element_by_class_name(
+                'product-tracking').get_attribute('data-track-products-array'))
+                            for item in items]
+            links = self._driver.find_elements_by_class_name(
+                'product-tile__details__info__name__link')
             
-            if len(self._driver.find_elements_by_class_name('search-no-results__section-title')):
+            if len(self._driver.find_elements_by_class_name(
+                'search-no-results__section-title')):
                 raise NoSearchResults
         
         df = pd.DataFrame()
@@ -96,9 +102,13 @@ class GroceryHelpersAPI:
                      'productVariant']:
             df[field] = [data[0][field] for data in product_data]
 
-        df['previouslyPurchased'] = ['Previously Purchased' in item.find_element_by_class_name('product-tile__eyebrow').text for item in items]
+        df['previouslyPurchased'] = ['Previously Purchased' in
+                                     item.find_element_by_class_name(
+                                         'product-tile__eyebrow'
+                                     ).text for item in items]
         df['link'] = [link.get_attribute('href') for link in links]    
-        df['categories'] = [urllib.request.unquote(link).replace('-', ' ').split('/')[4:-2] for link in df['link']]
+        df['categories'] = [urllib.request.unquote(link).replace('-', ' '). \
+                            split('/')[4:-2] for link in df['link']]
         
         unit_price_list = []
 
@@ -106,7 +116,8 @@ class GroceryHelpersAPI:
             unit_price = []
             for ul in item.find_elements_by_tag_name('ul'):
                 for li in ul.find_elements_by_tag_name('li'):
-                    data = [span.text for span in li.find_elements_by_tag_name('span')]
+                    data = [span.text for span in
+                            li.find_elements_by_tag_name('span')]
                     if data[-1] == 'ea'  and len(data) == 5 and data[2] == '(est.)' and data[3] == '(est.)':
                         price = data[1]
                         quantity = data[4]
@@ -179,19 +190,19 @@ class GroceryHelpersAPI:
         try:
             input_box = self._driver.find_element_by_class_name('quantity-selector__quantity__input')
             input_box.click()
+            ActionChains(self._driver).key_down(Keys.LEFT_CONTROL).send_keys('a').key_up(Keys.LEFT_CONTROL).perform()
             input_box.send_keys(0)
             input_box.send_keys(Keys.ENTER)
         except NoSuchElementException:
             pass        
         
-        buttons = self._driver.find_elements_by_tag_name('button')
-        button = [button for button in buttons if button.text == 'ADD'][0]
-        button.click()
-
+        self._driver.find_element_by_css_selector(
+            "button[data-track='productAddToCartButton']").click()
         time.sleep(1)
 
         input_box = self._driver.find_element_by_class_name('quantity-selector__quantity__input')
         input_box.click()
+        ActionChains(self._driver).key_down(Keys.LEFT_CONTROL).send_keys('a').key_up(Keys.LEFT_CONTROL).perform()
         input_box.send_keys(quantity)
         input_box.send_keys(Keys.ENTER)
         
@@ -242,20 +253,28 @@ class GroceryHelpersAPI:
 
             while time.time() - start_time < timeout:
                 try:
-                    product_descriptions = self._driver.find_elements_by_class_name('order-history-details-products__product__info__name')
+                    product_descriptions = self._driver. \
+                        find_elements_by_class_name(
+                        'order-history-details-products__product__info__name')
                 except NoSuchElementException:
                     pass
 
-            product_descriptions = [product_description.text for product_description in product_descriptions]
+            product_descriptions = [product_description.text for
+                                    product_description in product_descriptions]
 
-            product_skus = self._driver.find_elements_by_class_name('order-history-details-products__product__info__code')
+            product_skus = self._driver.find_elements_by_class_name(
+                'order-history-details-products__product__info__code')
             product_skus = [product_sku.text for product_sku in product_skus]
 
-            product_quantities = self._driver.find_elements_by_class_name('order-history-details-products__product__quantity')
-            product_quantities = [product_quantity.text for product_quantity in product_quantities]
+            product_quantities = self._driver.find_elements_by_class_name(
+                'order-history-details-products__product__quantity')
+            product_quantities = [product_quantity.text for product_quantity
+                                  in product_quantities]
 
-            product_prices = self._driver.find_elements_by_class_name('order-history-details-products__product__price')
-            product_prices = [float(product_price.text[1:]) for product_price in product_prices]
+            product_prices = self._driver.find_elements_by_class_name(
+                'order-history-details-products__product__price')
+            product_prices = [float(product_price.text[1:])
+                              for product_price in product_prices]
             
             df_products = self.get_product_list()
             
@@ -424,3 +443,99 @@ class ValumartAPI(GroceryHelpersAPI):
                        data_directory=data_directory, 
                        base_url='https://www.valumart.ca',
                        store_name='Valu-mart')
+
+        
+class WalmartAPI(GroceryHelpersAPI):
+    def __init__(self, user=None, password=None, user_data_dir=None,
+                 data_directory=os.path.join('.', 'data')):
+        super().__init__(user=user, password=password, user_data_dir=user_data_dir,
+                       data_directory=data_directory, 
+                       base_url='https://www.walmart.ca',
+                       store_name='Walmart')
+
+    @setup_and_teardown_driver
+    def search(self, term, timeout=10, follow_first_link=False):
+        self._driver.get('%s/search/%s' % (self._base_url, term))
+
+        descriptions = self._driver.find_elements_by_class_name('description')
+        descriptions = [description.text for description in descriptions]
+
+        titles = self._driver.find_elements_by_class_name('title')
+        titles = [title.text for title in titles][:len(descriptions)]
+        
+        price_units = self._driver.find_elements_by_class_name('price-unit')
+        price_units = [price_unit.text for price_unit in price_units]
+
+        prices = self._driver.find_elements_by_class_name('price-current')
+        prices = [price.text.replace('\n', '') for price in prices]
+        prices = [float(price[1:]) if price.find('¢') == -1 else float('0.%2d' % int(price[:-1])) for price in prices]
+
+        skus = self._driver.find_elements_by_class_name('productSkus')
+        data = [json.loads(sku.get_attribute('value')) for sku in skus]
+        skus = [item['productid'] for item in data]
+
+        links = self._driver.find_elements_by_class_name('product-link')
+        links = [self._base_url + link.get_attribute('data-bind').split(',')[1][2:-3] for link in links]
+
+        if follow_first_link and len(links):
+            self._driver.get(links[0])
+        
+        df = pd.DataFrame({
+         'sku': skus, 
+         'title': titles,
+         'description': descriptions,
+         'unit_price': price_units,
+         'price': prices,
+         'link': links})
+
+        return df
+    
+    
+    @setup_and_teardown_driver
+    def get_product_info(self, link=None, timeout=10):
+        if link and self._driver.current_url != link:
+            self._driver.get(link)
+        elif link is None:
+            link = self._driver.current_url
+
+        json_data = [json.loads(script.get_attribute('innerHTML')) for script
+                     in self._driver.find_elements_by_css_selector(
+                         "script[type='application/ld+json']")]
+        product = [x for x in json_data if x['@type'] == 'Product'][0]
+        categories = [y['item']['name'] for y in
+                      [x for x in json_data if x['@type'] == 'BreadcrumbList'
+                      ][0]['itemListElement']][1:]
+        ppu = self._driver.find_element_by_css_selector("span[data-automation='buybox-price-ppu']").text
+
+        product_ids = {}
+        div = self._driver.find_element_by_xpath("//*[contains(text(), 'Product Identifiers')]")
+        for i in range(3):
+            div = self._driver.execute_script("""
+                return arguments[0].nextElementSibling
+            """, div)
+            field, value = div.text.split('\n')
+            product_ids[field] = value
+
+        product_info = {
+            'name': product['name'],
+            'description': product['description'],
+            'brand': product['brand']['name'],
+            'price': product['offers']['price'],
+            'unit_price': ppu,
+            'categories': categories
+        }
+
+        product_info.update(product_ids)
+        
+        return product_info
+    
+    @setup_and_teardown_driver
+    def add_product_to_current_order(self, link, quantity=1, timeout=10):
+        self._driver.get(link)
+
+        input_box = self._driver.find_element_by_css_selector("span[data-automation='quantity']").find_element_by_tag_name('input')
+        input_box.click()
+        ActionChains(self._driver).key_down(Keys.LEFT_CONTROL).send_keys('a').key_up(Keys.LEFT_CONTROL).perform()
+        input_box.send_keys(5)
+
+        self._driver.find_element_by_xpath("//button[contains(text(), 'Add to cart')]").click()
